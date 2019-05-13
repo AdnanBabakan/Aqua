d11a29a95f58394afd85287f9779c197fac5ca6e
Adnan Babakan <babakan@ttbz.local>
2019-05-13 10:33:02 +0430

    Colored var_dump

--- routes/Web.php
+++ routes/Web.php
@@ -1,7 +1,8 @@
 <?php
 use \Aqua\Router as Router;
+use \Aqua\Core as Core;
 
 Router::route('/', 'Index@HomeController');
-// Router::route('/clear_cache', function() {
-//     \Aqua\Cache::clear_cache();
-// });
+Router::route('/debug', function() {
+    Core::var_dump(Core::config());
+});
